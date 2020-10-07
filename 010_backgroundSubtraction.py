ce1ba27788c77e5c0cb3556ff48bd112d0cf2bb0
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2020-10-08 07:36:40 +0800

    Update 010_backgroundSubtraction.py

--- 010_backgroundSubtraction.py
+++ 010_backgroundSubtraction.py
@@ -5,42 +5,40 @@ YouTube: https://www.youtube.com/channel/UCJ7GIy9ArKLwxcLqw0j4e6w
 Telegram: t.me/nikileeyyxx
 ______________________________
 '''
-'''
-Created by LynxDigital
-Please give credits for the code if used ^.^
-YouTube: https://www.youtube.com/channel/UCJ7GIy9ArKLwxcLqw0j4e6w
-Telegram: t.me/nikileeyyxx
-______________________________
-'''
 
 import numpy as np
 import cv2
 
 capture = cv2.VideoCapture("Road Clips/003 (NUS YIH).mp4")
+#insert the path to your own video here! 
 
-#kernel = cv2.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-#fgbg = cv2.bgsegm.BackgroundSubtractorGMG()
-#fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
-#fgbg = cv2.createBackgroundSubtractorKNN(detectShadows=True)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+foreBack = cv2.bgsegm.createBackgroundSubtractorMOG()
 
 while True:
     isSuccessful, frame = capture.read()
 
     if isSuccessful:
+
+        foregroundMask = foreBack.apply(frame)
+        morphMask = cv2.morphologyEx(foregroundMask, cv2.MORPH_OPEN, kernel)
         
-        fgmask = fgbg.apply(frame)
-        #fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+        percentScale = 0.3
+        widthFrame = int(morphMask.shape[1] * percentScale)
+        heightFrame = int(morphMask.shape[0] * percentScale)
+        dimensionFrame = (widthFrame, heightFrame)
+        scaledFrame = cv2.resize(morphMask, dimensionFrame, interpolation =cv2.INTER_AREA)
 
         percentScale = 0.3
-        widthFrame = int(fgmask.shape[1] * percentScale)
-        heightFrame = int(fgmask.shape[0] * percentScale)
+        widthFrame = int(foregroundMask.shape[1] * percentScale)
+        heightFrame = int(foregroundMask.shape[0] * percentScale)
         dimensionFrame = (widthFrame, heightFrame)
-        scaledFrame = cv2.resize(fgmask, dimensionFrame, interpolation =cv2.INTER_AREA)
+        scaledFrame2 = cv2.resize(foregroundMask, dimensionFrame, interpolation =cv2.INTER_AREA)
+                
+        cv2.imshow('Morph', scaledFrame)
+        cv2.imshow('Foreground', scaledFrame2)
+
         
-        cv2.imshow('Frame', scaledFrame)
-        #cv2.imshow('FG MASK Frame', fgmask)
-            
     else:
         break
 
