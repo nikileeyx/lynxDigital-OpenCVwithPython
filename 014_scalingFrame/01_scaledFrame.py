fb809fc5c164649ff0473e8720603e4bd6de4ebe
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2020-10-17 08:07:11 +0800

    Create 01_scaledFrame.py

---
+++ 014_scalingFrame/01_scaledFrame.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+def scaleFrame(frame, scaledPercentage):
+    percentScale = scaledPercentage
+    widthFrame = int(frame.shape[1] * percentScale)
+    heightFrame = int(frame.shape[0] * percentScale)
+    dimensionFrame = (widthFrame, heightFrame)
+    scaledFrame = cv2.resize(frame, dimensionFrame, interpolation =cv2.INTER_AREA)
+    return scaledFrame
