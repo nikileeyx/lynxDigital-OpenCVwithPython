296fd55a7f713062828420f241a748c46fb1a80e
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2020-10-31 20:20:55 +0800

    Update 01_scaledFrame.py

--- 014_scalingFrame/01_scaledFrame.py
+++ 014_scalingFrame/01_scaledFrame.py
@@ -6,6 +6,10 @@ This code alone will not execute any file, this is a file with functions that wi
 import cv2
 import numpy as np
 
+#Scaling Frame Function that takes two arguements, 
+#one is the frame and the second is the scaled frame as a percentage.
+#you can set "scaledPercentage = 50" if you want a frame
+
 def scaleFrame(frame, scaledPercentage):
     percentScale = scaledPercentage
     widthFrame = int(frame.shape[1] * percentScale)
