fce9a5ba542c8bf3e17de0709128aa048f647d6e
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2020-11-06 08:39:33 +0800

    Update and rename 012_colorPicker.py to 012_colorPicker/012_colorPicker.py

--- 012_colorPicker/012_colorPicker.py
+++ 012_colorPicker/012_colorPicker.py
@@ -3,7 +3,7 @@
 Created by LynxDigital
 Please give credits for the code if used ^.^
 YouTube: https://www.youtube.com/channel/UCJ7GIy9ArKLwxcLqw0j4e6w
-Telegram: t.me/nikileeyyxx
+Like, Share, SUBSCRIBE!
 ______________________________
 '''
 
