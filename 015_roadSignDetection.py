4f478300a29f8ba56371a40488516978458736d9
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2021-06-30 21:33:16 +0800

    Update 015_roadSignDetection.py

--- 015_roadSignDetection.py
+++ 015_roadSignDetection.py
@@ -1,2 +1,8 @@
+'''
+Created by LynxDigital
+Please give credits for the code if used ^.^
 
-#hello import the template
+YouTube: https://www.youtube.com/channel/UCJ7GIy9ArKLwxcLqw0j4e6w
+Telegram: t.me/nikileeyyxx
+______________________________
+'''
