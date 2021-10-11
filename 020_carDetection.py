99e1051f089bbd0b97c560496b20e2fa5e01e36b
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2021-10-11 23:54:17 +0800

    Update 020_carDetection.py

--- 020_carDetection.py
+++ 020_carDetection.py
@@ -1,6 +1,7 @@
 '''
 Created by LynxDigital
 Please give credits for the code if used ^.^
+
 YouTube: https://www.youtube.com/channel/UCJ7GIy9ArKLwxcLqw0j4e6w
 Telegram: t.me/nikileeyyxx
 ______________________________
